237ea5a750a1d1f11c2fc0b0e9ce062643b19b96
Yuragranovsky <yuragranovsky@gmail.com>
2023-11-12 16:06:36 +0200

    adds lombok and handling validation exceptions

--- src/main/java/telran/exceptions/GlobalExceptionsHandler.java
+++ src/main/java/telran/exceptions/GlobalExceptionsHandler.java
@@ -1,21 +1,40 @@
 package telran.exceptions;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import lombok.extern.slf4j.Slf4j;
+
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionsHandler {
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	ResponseEntity<String> handlerMethodArgument(MethodArgumentNotValidException e) {
+		List<ObjectError> errors = e.getAllErrors();
+		String body = errors.stream().map(err -> err.getDefaultMessage())
+				.collect(Collectors.joining(";"));
+		return errorResponse(body, HttpStatus.BAD_REQUEST);
+	}
+	private ResponseEntity<String> errorResponse(String body, HttpStatus status) {
+		log.error(body);
+		return new ResponseEntity<>(body, status);
+	}
 @ExceptionHandler({IllegalStateException.class})
 ResponseEntity<String> badRequest(IllegalStateException e) {
 	String message = e.getMessage();
-	return new ResponseEntity<String>(message, HttpStatus.BAD_REQUEST);
+	return errorResponse(message, HttpStatus.BAD_REQUEST);
 }
 @ExceptionHandler({NotFoundException.class})
 ResponseEntity<String> notFound(NotFoundException e) {
 	String message = e.getMessage();
-	return new ResponseEntity<String>(message, HttpStatus.NOT_FOUND);
+	return errorResponse(message, HttpStatus.NOT_FOUND);
 }
 
 }
